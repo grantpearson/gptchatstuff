88fbb80514092df21da225645d1f767e874420da
Grant Pearson <grant@grantpearson.com>
2022-12-11 17:44:29 -0500

    retries

--- Add-FunctionFromGPT.ps1
+++ Add-FunctionFromGPT.ps1
@@ -1,69 +1,53 @@
-# Load an API key from a file in the current directory
-function Load-ApiKey {
-    [CmdletBinding()]
-    param()
-  
-    $apiKeyFile = Join-Path -Path (Get-Location) -ChildPath 'api-key.txt'
-    $apiKey = Get-Content $apiKeyFile
-  
-    return $apiKey
+function Add-FunctionFromGPT {
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory=$true)]
+    [string] $Description,
+    [Parameter(Mandatory=$true)]
+    [string] $Name
+  )
+
+  # Add the prompt prefix to the description
+  $prompt = "write a PowerShell function named $Name (in the global scope by appending global: to the function name) that $Description"
+
+  # Get the function from GPT
+  $functionCode = Get-FunctionFromGPT -Prompt $prompt
+
+  # Try running the function code up to 5 times, fixing any errors that may occur
+  $attempts = 0
+  while($attempts -lt 5) {
+      try {
+          # Create a new function with the generated function code
+          Invoke-Expression $functionCode
+
+          # Output the generated function code
+          Write-Output $functionCode
+
+          # Return the function code if no errors occurred
+          return $functionCode
+      }
+      catch {
+          # Increment the number of attempts
+          $attempts++
+
+          # If this is the last attempt, pass the function code and the error message to GPT to generate a fixed version of the function
+          if ($attempts -eq 5) {
+              # Set the prompt for GPT to fix the function
+              $prompt = "fix the following PowerShell function: $functionCode
+                         error: $($_.Exception.Message)"
+
+              # Get the fixed function from GPT
+              $fixedFunctionCode = Get-FunctionFromGPT -Prompt $prompt
+
+              # Create a new function with the fixed function code
+              Invoke-Expression $fixedFunctionCode
+
+              # Output the fixed function code
+              Write-Output $fixedFunctionCode
+
+              # Return the fixed function code
+              return $fixedFunctionCode
+          }
+      }
   }
-  
-  # Send an HTTP POST request to the OpenAI API to generate a PowerShell function
-  function Get-FunctionFromGPT {
-    [CmdletBinding()]
-    param(
-      [Parameter(Mandatory=$true)]
-      [string] $Prompt
-    )
-  
-    # Load the API key from a file in the current directory
-    $apiKey = Load-ApiKey
-  
-    # Set the headers for the HTTP request
-    $headers = @{
-      'Content-Type' = 'application/json'
-      'Authorization' = "Bearer $apiKey"
-    }
-  
-    # Set the data for the HTTP request
-    $data = @{
-      'prompt' = $prompt
-      'max_tokens' = 1024
-      'n' = 1
-      'stop' = $null
-      'temperature' = 0.5
-      'model' = 'text-davinci-002'
-    }
-    $jsonData = $data | ConvertTo-Json
-  
-    # Send the HTTP POST request to the OpenAI API
-    $response = Invoke-WebRequest -Method Post -Uri 'https://api.openai.com/v1/completions' -Headers $headers -Body $jsonData
-  
-    # Parse the response and create a PowerShell function
-    $function = ($response.Content | ConvertFrom-Json).choices[0].text
-   
-    return $function
-  }
-  function Add-FunctionFromGPT {
-    [CmdletBinding()]
-    param(
-      [Parameter(Mandatory=$true)]
-      [string] $Description,
-      [Parameter(Mandatory=$true)]
-      [string] $Name
-    )
-  
-    # Add the prompt prefix to the description
-    $prompt = "write a PowerShell function named $Name (in the global scope by appending global: to the function name) that $Description"
-  
-    # Get the function from GPT
-    $functionCode = Get-FunctionFromGPT -Prompt $prompt
-  
-    # Create a new function with the generated function code
-    Invoke-Expression $functionCode
-  
-    # Output the generated function code
-    Write-Output $functionCode
-  }
-  
+}
